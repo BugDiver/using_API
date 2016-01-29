6deef5b59255f65e44a247e35ff09289827d1a67
BugDiver <vinaysh@thoughtworks.com>
2016-01-29 15:05:37 +0530

    [Vinay]:- list all repos of an user
    What changes have you made?
    
    * it,s a simple program to get list of all repos of an specified user.
    
    Why do we need this change?
    
    * just for learning API,s
    
    What can this change impact?
    
    * you can use it like a spy

---
+++ githubRepos.js
@@ -0,0 +1,18 @@
+var https = require('https');
+var url = "https://api.github.com/users/username/repos";
+var unirest = require('unirest');
+
+var showRepos = function(repoDetails){
+	repoDetails.forEach(function(repoDetail ,index){
+		console.log(index+1+':'	,repoDetail.name);
+	});
+}
+
+var getReposOf = function(username){var Request = unirest.get(url.replace('username',username));
+	var headers = {'Accept': 'application/json','User-Agent': 'Unirest Node.js'};
+	Request.headers(headers).end(function (response) {
+		showRepos(response.body);
+	});
+};
+
+getReposOf(process.argv[2]);
